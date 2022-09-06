51a5e034c4e910c11503249c8c875a41cb10f694
ItaloCobains <monokatchaleger@gmail.com>
2022-09-06 11:53:24 -0300

    More interceptor for prisma

--- src/common/errors/interceptors/database.interceptor.ts
+++ src/common/errors/interceptors/database.interceptor.ts
@@ -3,10 +3,13 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  BadRequestException,
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { isPrismaError } from '../../utils/is-prisma-error.util';
+import { DatabaseError } from '../types/DatabaseError';
+import { handleDatabaseErrors } from './../../utils/handle-database-erros.util';
 
 @Injectable()
 export class DatabaseInterceptor implements NestInterceptor {
@@ -14,8 +17,13 @@ export class DatabaseInterceptor implements NestInterceptor {
     return next.handle().pipe(
       catchError(error => {
         if (isPrismaError(error)) {
+          error = handleDatabaseErrors(error);
         }
-        throw error;
+        if (error instanceof DatabaseError) {
+          throw new BadRequestException(error.message);
+        } else {
+          throw error;
+        }
       }),
     );
   }
---
+++ src/common/utils/handle-database-erros.util.ts
@@ -0,0 +1,17 @@
+import { PrismaClientError } from './../errors/types/PrismaClientError';
+import { DatabaseError } from '../errors/types/DatabaseError';
+import { UniqueConstraintError } from './../errors/types/UniqueConstraintError';
+
+enum PrismaErrors {
+  UniqueConstraintFail = 'P2002',
+}
+
+export const handleDatabaseErrors = (e: PrismaClientError): Error => {
+  switch (e.code) {
+    case PrismaErrors.UniqueConstraintFail:
+      return new UniqueConstraintError(e);
+
+    default:
+      return new DatabaseError(e.message);
+  }
+};
